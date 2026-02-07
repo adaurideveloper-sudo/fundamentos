67b81c4f83ed112c479cc3cb34540edc2386eb1b
adauri.developer <adauri.developer@gmail.com>
2026-02-07 18:45:33 -0300

    modified:   .gitignore         modified:   src/basico/Array.java

--- src/basico/Array.java
+++ src/basico/Array.java
@@ -1,4 +1,20 @@
 package basico;
 
+import static java.lang.System.out;
+
+
 public class Array {
+
+    public static void main(String[] args) {
+
+        String [] ninja = new String[4];
+        ninja[0] = "Naruto Uzumaki";
+        ninja[1] = "Sasuke Uchiha";
+        ninja[2] = "Sakura Haruno";
+        ninja[3] = "Kakashi Hatake";
+
+        out.println(ninja);
+
+    }
+
 }
