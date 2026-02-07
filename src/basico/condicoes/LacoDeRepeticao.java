1146a4a2d2038bc92cc5158c69daa732bd22dc74
adauri.developer <adauri.developer@gmail.com>
2026-02-07 20:28:57 -0300

    modified:   src/basico/condicoes/LacoDeRepeticao.java

---
+++ src/basico/condicoes/LacoDeRepeticao.java
@@ -0,0 +1,21 @@
+package basico.condicoes;
+
+public class LacoDeRepeticao {
+    public static void main(String[] args) {
+
+        int numeroDeClones = 0;
+        int numeroMaximoDeClones = 40;
+
+
+        while (numeroDeClones < numeroMaximoDeClones) {
+            numeroDeClones ++;
+        }
+            System.out.println("O Naruto um clone das sombras " + numeroDeClones);
+
+
+        for (int i = 0; i <= numeroMaximoDeClones ; i++) {
+            System.out.println("O Naruto esta se clonando e ja se clonou " + i + " vezes");
+        }
+
+    }
+}
