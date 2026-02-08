bf3e21ae42ef69af3c42883a1c10ea4da967d3ea
adauri.developer <adauri.developer@gmail.com>
2026-02-07 21:10:56 -0300

    modified:   src/basico/Array.java

--- src/basico/Array.java
+++ src/basico/Array.java
@@ -7,13 +7,17 @@ public class Array {
 
     public static void main(String[] args) {
 
-        String [] ninja = new String[4];
+        String [] ninja = new String[6];
         ninja[0] = "Naruto Uzumaki";
         ninja[1] = "Sasuke Uchiha";
         ninja[2] = "Sakura Haruno";
         ninja[3] = "Kakashi Hatake";
+        ninja[4] = "Shikamaru Nara";
+        ninja[5] = "Hinata Hyuuga";
 
-        out.println(ninja);
+        for (int i = 0; i < 6; i++) {
+            out.println(ninja[i]);
+        }
 
     }
 
