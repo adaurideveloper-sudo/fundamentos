ff24ec7ed6d2830fcf2b877e61aaec484e84b922
adauri.developer <adauri.developer@gmail.com>
2026-02-20 17:52:48 -0300

    new file:   src/intermediario/listas/Main.java

---
+++ src/intermediario/listas/Main.java
@@ -0,0 +1,31 @@
+package intermediario.listas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    static void main(String[] args) {
+
+        List<String> ninjaList = new ArrayList<>();
+        ninjaList.add("Naruto Uzumaki");
+        ninjaList.add("Sasuke Uchiha");
+        ninjaList.add("Itachi Uchiha");
+        ninjaList.add("Tobirama Senju");
+        ninjaList.add("Kakashi Hatake");
+        ninjaList.set(3, "Hashirama Senju");
+        ninjaList.remove(4);
+        System.out.println(ninjaList);
+
+        for (int i = 0; i < ninjaList.toArray().length; i++) {
+            System.out.println(ninjaList.get(i));
+        }
+
+    }
+
+
+
+
+
+
+}
