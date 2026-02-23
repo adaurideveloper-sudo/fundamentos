b22145960a0b22f1b8bd2d2d8c1a6d8b496e0912
adauri.developer <adauri.developer@gmail.com>
2026-02-23 19:20:52 -0300

    modified:   src/intermediario/record/RecordNinja.java

--- src/intermediario/record/RecordNinja.java
+++ src/intermediario/record/RecordNinja.java
@@ -1,11 +1,6 @@
 package intermediario.record;
 
 public record RecordNinja(String nome, String email, int telefone) {
-    public RecordNinja(String nome, String email, int telefone) {
-        this.nome = nome;
-        this.email = email;
-        this.telefone = telefone;
-    }
 
     public RecordNinja cadastrarNinja(String nome, String email, int telefone) {
         RecordNinja novoNinja = new RecordNinja(nome, email, telefone);
